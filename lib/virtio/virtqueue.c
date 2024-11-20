arnopo/open-amp

virtqueue: Add helper to get the buffer address from descriptor index
As it possible to get the buffer length we need also to retrieve
the address associated to the descriptor index.
This is need by rpmsg virtio to implement the buffer recycler.

Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>

lib/virtio/virtqueue.c
@@ -226,6 +226,12 @@ uint32_t virtqueue_get_buffer_length(struct virtqueue *vq, uint16_t idx)
 	return vq->vq_ring.desc[idx].len;
 }
 
+void *virtqueue_get_buffer_addr(struct virtqueue *vq, uint16_t idx)
+{
+	VRING_INVALIDATE(vq->vq_ring.desc[idx].len);
+	return virtqueue_phys_to_virt(vq, vq->vq_ring.desc[idx].addr);
+}
+
 /**
  * virtqueue_free   - Frees VirtIO queue resources
  *

open-vela/nuttx_openamp_open-amp

lib/virtio/virtqueue.c
@@ -226,6 +226,12 @@ uint32_t virtqueue_get_buffer_length(struct virtqueue *vq, uint16_t idx)
 	return vq->vq_ring.desc[idx].len;
 }
 
+void *virtqueue_get_buffer_addr(struct virtqueue *vq, uint16_t idx)
+{
+	VRING_INVALIDATE(vq->vq_ring.desc[idx].addr);
+	return virtqueue_phys_to_virt(vq, vq->vq_ring.desc[idx].addr);
+}
+
 /**
  * virtqueue_free   - Frees VirtIO queue resources
  *