1a54fa2bb216520742152da2276a738744f8dbf6
Bowen Wang <wangbowen6@xiaomi.com>
2025-05-22 11:09:22 +0800

    lib/virtqueue.h: add virtqueue_dump when VQASSERT failed
    Dump more virtqueue information for the debug
    
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- lib/include/openamp/virtqueue.h
+++ lib/include/openamp/virtqueue.h
@@ -173,6 +173,7 @@ typedef void (*vq_notify)(struct virtqueue *);
 		if (!(_exp)) { \
 			metal_log(METAL_LOG_EMERGENCY, \
 				  "%s: %s - "_msg, __func__, (_vq)->vq_name); \
+			virtqueue_dump(_vq); \
 			metal_assert(_exp); \
 		} \
 	} while (0)
