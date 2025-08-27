5eb14341ac0a450fcd687000db5736278100d20c
Bowen Wang <wangbowen6@xiaomi.com>
2025-08-27 20:33:29 +0800

    lib/virtio/virtio: fix the coverity issue in function virtio_dev_name()
    ((MISRA C-2012 Rule 8.9)
    An object should be declared at block scope if its identifier only
    appears in a single function.
    
    (MISRA C-2012 Rule 10.3)
    The value of an expression shall not be assigned to an object with
    a narrower essential type or of a different essential type category.
    
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- lib/virtio/virtio.c
+++ lib/virtio/virtio.c
@@ -11,31 +11,9 @@
  * TODO :
  * This structure may change depending on the types of devices we support.
  */
-static const struct virtio_ident {
-	unsigned short devid;
+struct virtio_ident {
+	uint32_t devid;
 	const char *name;
-} virtio_ident_table[] = {
-	{
-	VIRTIO_ID_NETWORK, "Network"}, {
-	VIRTIO_ID_BLOCK, "Block"}, {
-	VIRTIO_ID_CONSOLE, "Console"}, {
-	VIRTIO_ID_ENTROPY, "Entropy"}, {
-	VIRTIO_ID_BALLOON, "Balloon"}, {
-	VIRTIO_ID_IOMEMORY, "IOMemory"}, {
-	VIRTIO_ID_SCSI, "SCSI"}, {
-	VIRTIO_ID_9P, "9P Transport"}, {
-	VIRTIO_ID_MAC80211_WLAN, "MAC80211 WLAN"}, {
-	VIRTIO_ID_RPROC_SERIAL, "Remoteproc Serial"}, {
-	VIRTIO_ID_GPU, "GPU"}, {
-	VIRTIO_ID_INPUT, "Input"}, {
-	VIRTIO_ID_VSOCK, "Vsock Transport"}, {
-	VIRTIO_ID_SOUND, "Sound"}, {
-	VIRTIO_ID_FS, "File System"}, {
-	VIRTIO_ID_MAC80211_HWSIM, "MAC80211 HWSIM"}, {
-	VIRTIO_ID_I2C_ADAPTER, "I2C Adapter"}, {
-	VIRTIO_ID_BT, "Bluetooth"}, {
-	VIRTIO_ID_GPIO, "GPIO" }, {
-	0, NULL}
 };
 
 static int virtio_create_virtqueues_default(struct virtio_device *vdev,
@@ -76,8 +54,32 @@ static int virtio_create_virtqueues_default(struct virtio_device *vdev,
 	return ret;
 }
 
-const char *virtio_dev_name(unsigned short devid)
+const char *virtio_dev_name(uint32_t devid)
 {
+	static const struct virtio_ident virtio_ident_table[] = {
+		{
+		VIRTIO_ID_NETWORK, "Network"}, {
+		VIRTIO_ID_BLOCK, "Block"}, {
+		VIRTIO_ID_CONSOLE, "Console"}, {
+		VIRTIO_ID_ENTROPY, "Entropy"}, {
+		VIRTIO_ID_BALLOON, "Balloon"}, {
+		VIRTIO_ID_IOMEMORY, "IOMemory"}, {
+		VIRTIO_ID_SCSI, "SCSI"}, {
+		VIRTIO_ID_9P, "9P Transport"}, {
+		VIRTIO_ID_MAC80211_WLAN, "MAC80211 WLAN"}, {
+		VIRTIO_ID_RPROC_SERIAL, "Remoteproc Serial"}, {
+		VIRTIO_ID_GPU, "GPU"}, {
+		VIRTIO_ID_INPUT, "Input"}, {
+		VIRTIO_ID_VSOCK, "Vsock Transport"}, {
+		VIRTIO_ID_SOUND, "Sound"}, {
+		VIRTIO_ID_FS, "File System"}, {
+		VIRTIO_ID_MAC80211_HWSIM, "MAC80211 HWSIM"}, {
+		VIRTIO_ID_I2C_ADAPTER, "I2C Adapter"}, {
+		VIRTIO_ID_BT, "Bluetooth"}, {
+		VIRTIO_ID_GPIO, "GPIO" }, {
+		0UL, NULL}
+	};
+
 	const struct virtio_ident *ident;
 	const char *name = NULL;
 
